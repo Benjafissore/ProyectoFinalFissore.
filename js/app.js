79cf07be1e3c24c56725be87705de5e4d3127bf3
Benjamin Fissore <benjafissore2000@gmail.com>
2024-11-26 01:13:02 +0000

    segunda preentrega

--- js/app.js
+++ js/app.js
@@ -16,5 +16,33 @@ function calcularTotal() {
     }
     return total;
 }
-
 console.log("El total del carrito es:$" + calcularTotal());
+
+
+class Producto {
+    constructor(id, imagen, nombreProducto, precio) {
+        this.id = id;
+        this.imagen = imagen;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+    }
+    obtenerDescripcion(){
+        return `${this.nombreProducto} cuesta $${this.precio}`;
+    }
+}
+
+
+const productos = [
+    new Producto(1, "./img/argentina-1985-1.jpg", "Los pumas", 25000),
+    new Producto(2, "./img/england-1980-1-500x500.jpg.webp", "Inglaterra", 20000),
+    new Producto(3, "./img/rugby017-new-zealand-1980-retrorugby.jpg", "All blacks", 26000),
+    new Producto(4, "./img/SaffaTeesSpringbokRugbyShirtProduct.webp", "Springbok", 27000),
+    new Producto(5, "./img/Polo_Rugby_Irlanda_Retrorugby.jpg", "Irlanda", 22000),
+    new Producto(6, "./img/rugby_007_scotland_retrorugby.jpg", "Escocia", 23000),
+    new Producto(7, "./img/rugby_023_italy_1975_retrorugby.jpg", "Italia", 23000),
+];
+
+productos.forEach(producto => {
+    console.log(producto.obtenerDescripcion());
+});
+
